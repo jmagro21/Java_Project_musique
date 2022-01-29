35a1359418e9475ea4a8d8c7befdcde81a7dcd05
jmagro21 <80267976+jmagro21@users.noreply.github.com>
2022-01-29 09:01:04 +0100

    Add files via upload

--- Playlist/BD.sql
+++ Playlist/BD.sql
@@ -1,23 +1,82 @@
-CREATE TABLE `jeu`.`utilisateur` (
-    `id` INT(11) NOT NULL AUTO_INCREMENT , 
-    `pseudo` TEXT NOT NULL ,
-    `mdp` TEXT NOT NULL ,
-    PRIMARY KEY (`id`)) 
-    ENGINE = InnoDB DEFAULT CHARSET=utf8mb4;
+-- phpMyAdmin SQL Dump
+-- version 4.5.4.1
+-- http://www.phpmyadmin.net
+--
+-- Client :  localhost
+-- Généré le :  Sam 29 Janvier 2022 à 07:59
+-- Version du serveur :  5.7.11
+-- Version de PHP :  5.6.18
 
-CREATE TABLE `chambre` (
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Base de données :  `playlist`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `musique`
+--
+
+CREATE TABLE `musique` (
   `id` int(11) NOT NULL,
   `nom` text NOT NULL,
-  `lit` int(3) NOT NULL,
-  `descript` longtext NOT NULL,
-  `special` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `dure` text NOT NULL,
+  `vote` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
 
-CREATE TABLE `reservation` (
+-- --------------------------------------------------------
+
+--
+-- Structure de la table `utilisateur`
+--
+
+CREATE TABLE `utilisateur` (
   `id` int(11) NOT NULL,
-  `id_utilisateur` int(11) NOT NULL,
-  `id_chambre` int(11) NOT NULL,
-  `dated` date NOT NULL,
-  `datef` date NOT NULL,
-  `paye` tinyint(1) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+  `pseudo` text NOT NULL,
+  `mdp` text NOT NULL,
+  `adm` int(11) NOT NULL,
+  `vote` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+--
+-- Index pour les tables exportées
+--
+
+--
+-- Index pour la table `musique`
+--
+ALTER TABLE `musique`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Index pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT pour les tables exportées
+--
+
+--
+-- AUTO_INCREMENT pour la table `musique`
+--
+ALTER TABLE `musique`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT pour la table `utilisateur`
+--
+ALTER TABLE `utilisateur`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
